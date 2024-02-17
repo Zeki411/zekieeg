6a972b6a9d1a12fb552b2306e35d34030d4f7e26
zeki411 <nguyenhatrung411@gmail.com>
2024-02-17 10:23:48 +0000

    up

--- transforms/label/category.py
+++ transforms/label/category.py
@@ -4,8 +4,90 @@ from torcheeg.transforms import LabelTransform
 
 class CategoryRange(LabelTransform):
     r'''
+    Categorize the label according to a certain threshold range. \
+    Labels within the range are set to the corresponding category, and labels outside the range are set to the last category.
+    
+    :obj:`CategoryRange` allows simultaneous categorization using the same threshold range for multiple labels. \
+    The threshold range can also be defined as a dictionary, where the key is the category name and the value is the threshold range. \
+    The last category is the category for labels that do not satisfy any threshold range ('others' by default if dict provided).
+    The range is defined as a list of two elements, where the first element is the lower bound and the second element is the upper bound. 
+
+    The threshold range can also be defined as a list of threshold ranges, where the last category is the category for labels that do not satisfy any threshold range.
+
+    .. code-block:: python
+    
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = { 
+                'High Stress' : [ [ [7.5, 9] ] ],
+                'Low Stress'  : [ [ [5, 7.5] ] ],
+            }
+        )
+        transform(y=[8])['y']
+        >>> High Stress
+    
+
+    .. code-block:: python
+
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = { 
+                'High Stress' : [ [ [7.5, 9] ], [ [0, 2.5]   ] ],
+                'Low Stress'  : [ [ [5, 7.5] ], [ [2.5, 5.0] ] ],
+            }
+        )
+        transform(y=[8, 2])['y']
+        >>> High Stress
+
+    .. code-block:: python
+
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = { 
+                'High Stress' : [ [ [7.5, 9] ], [ [0, 2.5]   ] ],
+                'Low Stress'  : [ [ [5, 7.5] ], [ [2.5, 5.0] ] ],
+            }
+        )
+        transform(y=[3, 2])['y']
+        >>> others
+
+    .. code-block:: python
+
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = { 
+                'High Stress' : [ [ [7.5, 9] ], [ [0, 2.5]   ] ],
+                'Low Stress'  : [ [ [5, 7.5] ], [ [2.5, 5.0] ] ],
+                'Remain'      : -1
+            }
+        )
+        transform(y=[3, 2])['y']
+        >>> Remain
+
+    .. code-block:: python
+
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = [
+                [ [ [7.5, 9] ], [ [0, 2.5]   ] ],
+                [ [ [5, 7.5] ], [ [2.5, 5.0] ] ],
+            ]
+        )
+        transform(y=[8, 2])['y']
+        >>> 0
+
+    .. code-block:: python
+
+        transform = ztransforms.CategoryRange(
+            threshold_ranges = [
+                [ [ [7.5, 9] ], [ [0, 2.5]   ] ],
+                [ [ [5, 7.5] ], [ [2.5, 5.0] ] ],
+            ]
+        )
+        transform(y=[3, 2])['y']
+        >>> 2
+
     Args:
-        threshold_ranges: list of threshold range for each attribute of sample original label to categorize
+        threshold_ranges (list or dict): The threshold range used during categorization. \
+                                        The range is defined as a list of two elements, \
+                                        where the first element is the lower bound and the second element is the upper bound. \
+                                        The last category is the category for labels that do not satisfy any threshold range.
+
     '''
 
     def __init__(self, threshold_ranges: Union[List, Dict]):
@@ -14,13 +96,25 @@ class CategoryRange(LabelTransform):
 
         if isinstance(threshold_ranges, dict):
             self.categories = list(threshold_ranges.keys())
-            self.threshold_ranges = [threshold_ranges[cat] for cat in self.categories]
-            self.categories.append('others')
+            self.remain_category = None
+            self.threshold_ranges = []
+            
+            for cat in self.categories:
+                if threshold_ranges[cat] == -1:
+                    self.remain_category = cat
+                    self.categories.remove(cat)
+                    break
+
+                self.threshold_ranges.append(threshold_ranges[cat])
+
+
+            if self.remain_category:
+                self.categories.append(self.remain_category)
+            else:
+                self.categories.append('others')
         else:
             self.threshold_ranges = threshold_ranges
             self.categories = list(range(len(threshold_ranges)+1))
-
-        # self.num_binaries = len(threshold_ranges) #TODO
 
     def __call__(self, *args,  y: Union[int, float, List],
                  **kwargs) -> Union[int, List]:
